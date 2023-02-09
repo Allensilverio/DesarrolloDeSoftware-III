aa530fd476050bd638b73f720ace7b737fabe102
Allen <1104220@est.intec.edu.do>
2023-02-09 13:27:32 -0400

    Se volvio loca la pc

--- TeoriaAsignacion1/Program.cs
+++ TeoriaAsignacion1/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +13,21 @@ namespace TeoriaAsignacion1
         static void Main(string[] args)
         {
             Cliente cliente = new Cliente();
-            cliente.ID = 1;
+            //cliente.ID = 1;
             cliente.Nombres = "Allen Radhames";
             cliente.Apellidos = "Silverio Olivo";
             cliente.Estado = 0;
-            cliente.FechaNacimiento = DateTime.Parse("2004-01-06");
+            cliente.FechaNacimiento = DateTime.Parse("2004-06-01");
 
             Console.WriteLine($"NOMBRES: {cliente.Nombres}- APELLIDOS:{cliente.Apellidos}");
-            Console.ReadLine();
+
+            SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\carim\\OneDrive\\Escritorio\\Allen\\TeoriaAsignacion1\\TeoriaAsignacion1\\MyData.mdf;Integrated Security=True");
+            connection.Open();
+            Console.WriteLine(connection.State);
+
+            SqlCommand command = new SqlCommand($"INSERT tblClientes(NOMBRES, APELLIDOS, FECHANACIMIENTO, SEXO, COMENTARIO) VALUES ('{cliente.Nombres}', '{cliente.Apellidos}', '{cliente.FechaNacimiento}', 'M', 'COMENTARIO')", connection);
+            
+             command.ExecuteNonQuery();
         }
     }
 }
