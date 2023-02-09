041706013272e0075670b3d277698a3f106e005c
Allen <1104220@est.intec.edu.do>
2023-02-09 09:30:25 -0400

    Agregar archivos de proyecto.

---
+++ TeoriaAsignacion1/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeoriaAsignacion1
+{
+    internal class Cliente
+    {
+        public int ID { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public int Estado { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+
+    }
+}
---
+++ TeoriaAsignacion1/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeoriaAsignacion1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente = new Cliente();
+            cliente.ID = 1;
+            cliente.Nombres = "Allen Radhames";
+            cliente.Apellidos = "Silverio Olivo";
+            cliente.Estado = 0;
+            cliente.FechaNacimiento = DateTime.Parse("2004-01-06");
+
+            Console.WriteLine($"NOMBRES: {cliente.Nombres}- APELLIDOS:{cliente.Apellidos}");
+            Console.ReadLine();
+        }
+    }
+}
