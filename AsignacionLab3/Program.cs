1a23749f4eebd8b33e43191803729cf31b0a477e
Allen <1104220@est.intec.edu.do>
2023-02-16 19:22:25 -0400

    Creacion asignacion 3 Lab Desarrollo de Software

---
+++ AsignacionLab3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsignacionLab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Run = true;
+            string opcion;
+
+            // Conexion con la Base de Datos
+
+            SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Cn"].ConnectionString);
+            connection.Open(); // abro la conexion con la base de datos
+            Console.WriteLine(connection.State);
+
+
+
+
+
+
+
+            while (Run)
+            {
+                // CONDICION DE SALIDA DEL BUCLE
+                Console.WriteLine("Quieres salir (s/n): ");
+                opcion = Console.ReadLine();
+
+                if (opcion.ToLower() == "s")
+                {
+                    Run = false;
+                }
+                else
+                {
+                    Run = true;
+                }
+
+            }
+        }
+    }
+}
