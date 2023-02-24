afec8a3528b4c7da960071239cf88063b8a7fdf0
Allen <1104220@est.intec.edu.do>
2023-02-23 22:10:06 -0400

    aaa

---
+++ Lab4/Feligreses.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    internal class Feligreses
+    {
+        public int id { get; set; }
+        public int tipodocumento { get; set; }
+        public string documento { get; set; }
+        public string nombres { get; set; }
+
+        public string apellidos { get; set; }
+
+        public DateTime fechaNacimiento { get; set; }
+
+        public DateTime fechaUltimaConfecion { get; set; }
+
+        public DateTime fechaUltimaVisita { get; set; }
+
+        public string estadoCivil { get; set; }
+        public string sexo { get; set; }
+
+        public int diezma { get; set; }
+
+        public int PerteneceAComunidad { get; set; }
+
+  
+
+
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblEventos]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [TipoDocumento] NVARCHAR(50) NOT NULL, 
+    [Documento] NVARCHAR(50) NOT NULL, 
+    [Nota] NVARCHAR(50) NOT NULL, 
+    [Descripcion] NVARCHAR(50) NOT NULL, 
+    [TipoEvento] INT NOT NULL
+)
